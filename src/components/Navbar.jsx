7dac5993e4093189346134fff197038ce63ebc4e
Melkibson <melkiyamna@protonmail.com>
2024-01-06 23:48:08 +0100

    Make the navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ import SearchBox from './Navbar/SearchBox'
 
 const Navbar = ({onChange}) => {
     return (
-      <div id="nav" className="w-100 bg-lightest-blue o-90 h4 flex items-center justify-around">
+      <div 
+        id="nav" 
+        className="w-100 bg-lightest-blue o-90 h4 
+        flex items-center justify-around -m flex-wrap justify-center">
         <MainTitle label="RobotFriends" />
         <SearchBox searchChange={onChange} />
       </div>
